23265fa5b33528cbe754814495ad1cf7fefef61d
patel-zeel <patel_zeel@iitgn.ac.in>
2022-06-13 09:46:33 +0530

    added coin toss laplax

---
+++ laplax/utils.py
@@ -0,0 +1,21 @@
+import jax
+import optax
+
+
+def train_model(model, data, optimizer, n_epochs, seed):
+    params = model.init(seed)
+    value_and_grad_fn = jax.value_and_grad(model.loss_fn)
+    state = optimizer.init(params)
+
+    @jax.jit
+    def one_step(params_and_state, xs):
+        params, state = params_and_state
+        loss, grads = value_and_grad_fn(params, data)
+        updates, state = optimizer.update(grads, state)
+        params = optax.apply_updates(params, updates)
+        return (params, state), loss
+
+    params_and_states, losses = jax.lax.scan(
+        one_step, (params, state), xs=None, length=n_epochs
+    )
+    return params_and_states[0], losses
